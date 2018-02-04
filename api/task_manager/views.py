85a5173afc33fb3c1bf8f77d76508598670fd1c6
Quentin McRee <qmcree@gmail.com>
2018-02-03 19:00:01 -0600

    Implemented other view functions.

--- api/task_manager/views.py
+++ api/task_manager/views.py
@@ -1,19 +1,51 @@
 from django.http import JsonResponse, HttpRequest
+from django.shortcuts import get_object_or_404
 
-from .models import Task
+from .models import Task, User
 
 
 def list_users(request: HttpRequest):
-    pass
+    users = User.objects.all()
+    data = list(
+        dict(
+            id=user.id,
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            address=dict(
+                street=user.address.street,
+                city=user.address.city,
+                state=user.address.state,
+                postal_code=user.address.postal_code,
+            ),
+        ) for user in users
+    )
+
+    return JsonResponse(dict(users=data))
 
 
 def list_users_tasks(request: HttpRequest, user_id: int):
-    pass
+    tasks = Task.objects.filter(assignee_user_id=user_id)
+    data = list(
+        dict(
+            id=task.id,
+            assignee_user_id=task.assignee_user_id,
+            title=task.title,
+            details=task.details,
+            priority_level=task.priority,
+            priority_label=task.get_priority_label(),
+            is_completed=task.is_completed,
+            is_accepted=task.is_accepted,
+            created_at=task.created_at,
+        ) for task in tasks
+    )
+
+    return JsonResponse(dict(tasks=data))
 
 
 def get_task_details(request: HttpRequest, task_id: int):
-    # TODO: catch DoesNotExist
-    task = Task.objects.get(id=task_id)
+    task = get_object_or_404(Task, id=task_id)
     notes = task.tasknote_set.all()
 
     notes_data = list(
